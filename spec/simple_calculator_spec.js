409ea9a3d79486ee6a1791f8b113cf8565ff263c
Thabiso Makolana <makolanathabiso@gmail.com>
2020-04-29 11:03:31 +0200

    added 4 more specs to spec.js test file

--- spec/simple_calculator_spec.js
+++ spec/simple_calculator_spec.js
@@ -1,12 +1,29 @@
-describe("Calculate",()=>{
-    var add  = require("../src/simple_calculator")
-    var multiply  = require("../src/simple_calculator")
+var add = require("../src/simple_calculator")
+var multiply = require("../src/simple_calculator")
 
-    it("should add two intergers 6 and 9"),()=>{
-        expect(add(6,9)).toEqual(15)
+describe("Calculate", () => {
+
+    it("should add two intergers 6 and 9"), () => {
+        expect(add(6, 9)).toEqual(15)
     }
 
-    it("should multiply two intergers 6 and 11"),()=>{
-        expect(multiply(6,11)).toEqual(66)
+    it("should add three intergers 6,4 and 9"), () => {
+        expect(add(6,4,9)).toEqual(19)
+    }
+
+    it("should add four intergers 6,4,5 and 9"), () => {
+        expect(add(6,4,5,9)).toEqual(24)
+    }
+
+    it("should multiply two intergers 6 and 11"), () => {
+        expect(multiply(6, 11)).toEqual(66)
+    }
+
+    it("should multiply three intergers 6,4 and 11"), () => {
+        expect(multiply(6,4,11)).toEqual(264)
+    }
+
+    it("should multiply four intergers 6,4,5 and 11"), () => {
+        expect(multiply(6,4,5,11)).toEqual(1320)
     }
 })
--- src/simple_calculator.js
+++ src/simple_calculator.js
@@ -1,23 +1,20 @@
-function add (){
+function add() {
     let sum = 0
-    for(let x=0;x<arguments.length;x++){
-        sum+=arguments[x]
+    for (let x = 0; x < arguments.length; x++) {
+        sum += arguments[x]
     }
 
     return sum
 }
 
-function multiply (){
+function multiply() {
     let sum = 1
-    for(let x=0;x<arguments.length;x++){
-        sum*=arguments[x]
+    for (let x = 0; x < arguments.length; x++) {
+        sum *= arguments[x]
     }
 
     return sum
 }
 
-console.log(add(3,5,4,6))
-console.log(multiply(3,5,4,6))
-
-module.exports =add
-module.exports =multiply
+module.exports = add
+module.exports = multiply
