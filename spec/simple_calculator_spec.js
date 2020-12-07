5ac01b25722994017da0412e3456c1d0aae6b7e1
Thabiso Makolana <makolanathabiso@gmail.com>
2020-12-07 14:16:06 +0200

    added readme

--- spec/simple_calculator_spec.js
+++ spec/simple_calculator_spec.js
@@ -1,32 +1,31 @@
-var add = require("../src/simple_calculator")
-var multiply = require("../src/simple_calculator")
+var { add, multiply } = require("../src/simple_calculator")
 
 describe("add", () => {
 
-    it("should add two intergers 6 and 9"), () => {
-        expect(add(6, 9)).toEqual(15)
-    }
+    it("should add two intergers 6 and 9", () => {
+        expect(add(6, 9)).toEqual(15);
+    })
 
-    it("should add three intergers 6,4 and 9"), () => {
-        expect(add(6, 4, 9)).toEqual(19)
-    }
+    it("should add three intergers 6,4 and 9", () => {
+        expect(add(6, 4, 9)).toEqual(19);
+    })
 
-    it("should add four intergers 6,4,5 and 9"), () => {
-        expect(add(6, 4, 5, 9)).toEqual(24)
-    }
+    it("should add four intergers 6,4,5 and 9", () => {
+        expect(add(6, 4, 5, 9)).toEqual(24);
+    })
 })
 
 describe("multiply", () => {
 
-    it("should multiply two intergers 6 and 11"), () => {
-        expect(multiply(6, 11)).toEqual(66)
-    }
+    it("should multiply two intergers 6 and 11", () => {
+        expect(multiply(6, 11)).toEqual(66);
+    })
 
-    it("should multiply three intergers 6,4 and 11"), () => {
-        expect(multiply(6, 4, 11)).toEqual(264)
-    }
+    it("should multiply three intergers 6,4 and 11", () => {
+        expect(multiply(6, 4, 11)).toEqual(264);
+    })
 
-    it("should multiply four intergers 6,4,5 and 11"), () => {
-        expect(multiply(6, 4, 5, 11)).toEqual(1320)
-    }
+    it("should multiply four intergers 6,4,5 and 11", () => {
+        expect(multiply(6, 4, 5, 11)).toEqual(1320);
+    })
 })
